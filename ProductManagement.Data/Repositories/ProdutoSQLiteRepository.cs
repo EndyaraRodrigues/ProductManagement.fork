f180bd2b96866df63ab08a94df539d3ffdbc648b
Endyara <endyara.rodrigues.313259810@msft.cesae.pt>
2026-06-09 15:17:48 +0100

    Refatorização da classe ProdutoSqlite

--- ProductManagement.Data/Repositories/ProdutoSQLiteRepository.cs
+++ ProductManagement.Data/Repositories/ProdutoSQLiteRepository.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ProductManagement.Data.Repositories
 {
@@ -12,31 +13,46 @@ namespace ProductManagement.Data.Repositories
     {
         private string _connectionString = "Data Source = produtos.db";
 
+        //adicionar para utilizar na base de dados in-memory : 
+        private readonly SqliteConnection _connection;
+
         //criação da tabela
 
         public ProdutoSQLiteRepository()
         {
-            using var con = new SqliteConnection(_connectionString);
-            con.Open();
+            //using var con = new SqliteConnection(_connectionString);
+            //con.Open();
+
+            _connection = new SqliteConnection(_connectionString);
+            _connection.Open();
 
             string sql = @"CREATE TABLE IF NOT EXISTS Produtos (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 Nome TEXT NOT NULL,
 Preco REAL NOT NULL);";
 
-            using var cmd = new SqliteCommand(sql, con);
+            using var cmd = new SqliteCommand(sql, _connection);
             cmd.ExecuteNonQuery();
         }
+
+        //*******************************************************************************
+            //adicionar construtor para testes (usa a connection que vem de fora):
+
+           public ProdutoSQLiteRepository (SqliteConnection connection)
+            {
+            _connection = connection;
+            }
+        
         //implementação do smetodos da interface
 
         public void Adicionar(Produto produto)
         {
-            using var con = new SqliteConnection(_connectionString);
-            con.Open();
+            //using var con = new SqliteConnection(_connectionString);
+            //con.Open();
 
             string sql = @"INSERT INTO Produtos (Nome,Preco) VALUES(@n,@p)";
 
-            using var cmd = new SqliteCommand(sql, con);
+            using var cmd = new SqliteCommand(sql, _connection);
 
             cmd.Parameters.AddWithValue("@n", produto.Nome);
             cmd.Parameters.AddWithValue("@p", produto.Preco);
@@ -49,12 +65,12 @@ Preco REAL NOT NULL);";
         {
             List<Produto> lista = new();
 
-            using var con = new SqliteConnection(_connectionString);
-            con.Open();
+            //using var con = new SqliteConnection(_connectionString);
+            //con.Open();
 
             string sql = @"SELECT Id, Nome, Preco FROM  Produtos";
 
-            using var cmd = new SqliteCommand(sql, con);
+            using var cmd = new SqliteCommand(sql, _connection);
 
             using var reader = cmd.ExecuteReader();
 
@@ -73,13 +89,13 @@ Preco REAL NOT NULL);";
 
         public Produto? ObterPorNome(string nome)
         {
-            using var con = new SqliteConnection(_connectionString);
-            con.Open();
+            //using var con = new SqliteConnection(_connectionString);
+            //con.Open();
 
             string sql = @"SELECT Id, Nome, Preco FROM  Produtos
 WHERE Nome=@n";
 
-            using var cmd = new SqliteCommand(sql, con);
+            using var cmd = new SqliteCommand(sql, _connection);
             cmd.Parameters.AddWithValue("@n", nome);
             using var reader = cmd.ExecuteReader();
 
@@ -98,23 +114,23 @@ WHERE Nome=@n";
 
         public bool Remover(int id)
         {
-            using var con = new SqliteConnection(_connectionString);
-            con.Open();
+            //using var con = new SqliteConnection(_connectionString);
+            //con.Open();
 
             string sql = @"DELETE FROM Produtos WHERE Id = @id";
 
-            using var cmd = new SqliteCommand(sql, con);
+            using var cmd = new SqliteCommand(sql, _connection);
             cmd.Parameters.AddWithValue("@id", id);
             int linhas = cmd.ExecuteNonQuery();
             return linhas > 0;
         }
         public bool ExistePorNome(string nome)
         {
-            using var con = new SqliteConnection(_connectionString);
-            con.Open();
+            //using var con = new SqliteConnection(_connectionString);
+            //con.Open();
 
             string sql = @"SELECT COUNT(*) FROM Produtos WHERE Nome=@n";
-            using var cmd = new SqliteCommand(sql, con);
+            using var cmd = new SqliteCommand(sql, _connection);
             cmd.Parameters.AddWithValue("@n", nome);
 
             long count = (long)cmd.ExecuteScalar();
